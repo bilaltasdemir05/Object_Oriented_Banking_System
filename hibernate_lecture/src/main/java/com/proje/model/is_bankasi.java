64c867a28a54ec754b7087026893e09816b4cf0d
Bilal Tasdemir <tsdmr.bilal@gmail.com>
2019-10-27 18:28:53 +0300

    is_bankasina_yeni_metodlar_eklendi

--- hibernate_lecture/src/main/java/com/proje/model/is_bankasi.java
+++ hibernate_lecture/src/main/java/com/proje/model/is_bankasi.java
@@ -2,7 +2,7 @@ package com.proje.model;
 
 
 public class is_bankasi implements banka {
-
+	private float kazanc;
 	@Override
 	public void ev_kredisi() {
 		// TODO Auto-generated method stub
@@ -31,6 +31,14 @@ public class is_bankasi implements banka {
 	public void faiz_hesapla() {
 		// TODO Auto-generated method stub
 		
+	}
+	public float kazanc_hesapla(ögrenci ogr) {
+		kazanc=(float) (ogr.getBakiye()*0.02);
+		return kazanc;		
+	}
+	public float kazanc_hesapla(emekli emk) {
+		kazanc=(float) (emk.getBakiye()*0.01);
+		return kazanc;
 	}
 
 }
